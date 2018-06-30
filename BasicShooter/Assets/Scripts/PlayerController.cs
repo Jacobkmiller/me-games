273adf6f2d81e65b3701f1181ddca49803047311
Zak Espley <zespley@gmail.com>
2018-06-30 14:17:44 -0700

    Updated PlayerController

--- BasicShooter/Assets/Scripts/PlayerController.cs
+++ BasicShooter/Assets/Scripts/PlayerController.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(PlayerMotor))]
-public class PlayerMotor : MonoBehaviour {
+public class PlayerController : MonoBehaviour {
 
 	[SerializeField]
 	private float speed = 5f;
+	[SerializeField]
+	private float lookSensitivity = 3f;
 	private PlayerMotor motor;
 
 	void Start()
@@ -26,6 +28,24 @@ public class PlayerMotor : MonoBehaviour {
 		//Final Movement velcoty
 		Vector3 _velocity = (_movHorizontal + _movVertical).normalized * speed;
 		
+		//Apply movement
 		motor.Move(_velocity);
+
+		//Calculate rotation as a 3D vector (Turning around)
+		float _yRot = Input.GetAxisRaw("Mouse X");
+
+		Vector3 _rotation = new Vector3(0f, _yRot, 0f) * lookSensitivity;
+
+		//Apply rotation
+		// motor.Rotate(_rotation);
+
+		//Calculate camera rotation as a 3D vector (Looking around)
+		float _xRot = Input.GetAxisRaw("Mouse Y");
+
+		Vector3 _cameraRotation = new Vector3(_xRot, 0f, 0f) * lookSensitivity;
+		
+		//Apply camera rotation
+		// motor.RotateCamera(_cameraRotation);
+
 	}
 }
