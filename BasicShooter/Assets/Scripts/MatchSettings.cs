22a8c689fbfc0568a2089ec0dbc60ac6344423d2
Zak Espley <zespley@gmail.com>
2018-06-30 23:39:18 -0700

    Added Death and Respawn

---
+++ BasicShooter/Assets/Scripts/MatchSettings.cs
@@ -0,0 +1,6 @@
+﻿[System.Serializable]
+
+public class MatchSettings : MonoBehaviour {
+
+	public float respawnTime = 3f;
+}
--- BasicShooter/Assets/Scripts/Player.cs
+++ BasicShooter/Assets/Scripts/Player.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using UnityEngine.Networking;
+using System.Collections;
 
 public class Player : NetworkBehaviour {
 
@@ -8,18 +9,71 @@ public class Player : NetworkBehaviour {
 	private int maxHealth = 100;
 	[SyncVar]
 	private int currentHealth;
+	[SyncVar]
+	private bool _isDead = false;
+	public bool isDead {
+		get {return _isDead;}
+		protected set {_isDead = value;}
+	}
+	[SerializeField]
+	private Behaviour[] disableOnDeath;
+	private bool[] wasEnabled;
 
-	void Awake() {
+	public void Setup() {
+		wasEnabled = new bool[disableOnDeath.Length];
+		for (int i =0; i < wasEnabled.Length; i++){
+			wasEnabled[i] = disableOnDeath[i].enabled;
+		}
 		SetDefaults();
 	}
 
+	private IEnumerator Respawn() {
+		yield return new WaitForSeconds(GameManager.instance.matchSettings.respawnTime);
+		SetDefaults();
+		Transform _spawnPoint = NetworkManager.singleton.GetStartPosition();
+		transform.position = _spawnPoint.position;
+		transform.rotation = _spawnPoint.rotation;
+	}
 	public void SetDefaults() {
 		currentHealth = maxHealth;
-	}
+		isDead = false;
+		for (int i = 0; i < disableOnDeath.Length; i++){
+			 disableOnDeath[i].enabled = wasEnabled[i];
+		}
 
-	public void TakeDamage(int _amount) {
+		Collider _col = GetComponent<Collider>();
+		if (_col != null){
+			_col.enabled = true;
+		}
+	}
+	[ClientRpc]
+	public void RpcTakeDamage(int _amount) {
+		if (isDead) {
+			return;
+		}
 		currentHealth -= _amount;
 
 		Debug.Log(transform.name + " now has " + currentHealth + " health!");
+
+		if (currentHealth <= 0) {
+			Die();
+		}
+	}
+	public void Die() {
+		isDead = true;
+		//DISABLE COMPONENTS
+		for (int i = 0; i < disableOnDeath.Length; i++){
+			disableOnDeath[i].enabled = false;
+		}
+
+		Collider _col = GetComponent<Collider>();
+		if (_col != null) {
+			_col.enabled = false;
+		}
+
+		Debug.Log(transform.name + " is dead!");
+		//RESPAWN
+		StartCoroutine(Respawn());
+
 	}
 }
