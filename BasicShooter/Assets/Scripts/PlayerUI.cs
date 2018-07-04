aef832baf96e21a9bb6e16ecc1bcd2eece1c0461
jacobkmiller <jacobkmiller@icloud.com>
2018-07-03 19:03:53 -0700

    fixed merge conflicts

---
+++ BasicShooter/Assets/Scripts/PlayerUI.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerUI : MonoBehaviour {
+
+	[SerializeField]
+	private Text healthText;
+	private Player player;
+
+	/// <summary>
+	/// Start is called on the frame when a script is enabled just before
+	/// any of the Update methods is called the first time.
+	/// </summary>
+	void Start() {
+		// UI = Instantiate(playerUIPrefab);
+		// UI.name = playerUIPrefab.name;
+	}
+
+	public void SetPlayer(Player _player) {
+		player = _player;
+	}
+
+	public void SetHealth(int value){
+		// UI.GetComponentInChildren<Text>().text = value + "%";
+		healthText.text = value + "%";
+	}
+
+	
+
+
+}
+
